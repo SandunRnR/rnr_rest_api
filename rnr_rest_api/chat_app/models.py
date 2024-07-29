35d8f94294e9c14c245592253b7942ade4c6d3d9
IsuruRNR <isuru.rnrsolutions@gmail.com>
2024-07-29 11:03:41 +0530

    Added chatapp

--- rnr_rest_api/chat_app/models.py
+++ rnr_rest_api/chat_app/models.py
@@ -3,9 +3,11 @@ from django.utils import timezone
 
 class ChatMessage(models.Model):
     patient_message = models.TextField(max_length=255, default='null')
+    device_id = models.CharField(max_length=255, default='null')
     doctor_response = models.TextField(max_length=255, default='null')
     next_action = models.CharField(max_length=20, default='null') 
     created_at = models.DateTimeField(default=timezone.now)  
 
     def __str__(self):
         return f"ChatMessage({self.patient_message}, {self.doctor_response}, {self.next_action})"
+    
